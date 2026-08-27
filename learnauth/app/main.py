ae9f081343ea67220efaa909f4aa7b74fc8e9db6
Kunal Sambyal <kunalsambyal9628@gmail.com>
2026-08-27 07:58:47 +0530

    feat: add CORSMiddleware

--- learnauth/app/main.py
+++ learnauth/app/main.py
@@ -13,7 +13,17 @@ async def lifespan(app: FastAPI):
     yield
 
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
